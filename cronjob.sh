39fc6161d8bc59dc35ace570480e1a478309798e
Glen Pitt-Pladdy <gpp-hg@pitt-pladdy.com>
2012-10-06 15:53:00 +0100

    Added total memory to graph

--- cronjob.sh
+++ cronjob.sh
@@ -4,9 +4,11 @@ vmstat () {
 		| /bin/sed 's/^  *//' \
 		| /bin/sed 's/  */\n/g' \
 		>/var/local/snmp/vmstat.tmp$$
+	/bin/grep '^MemTotal:' /proc/meminfo \
+		| /bin/sed 's/^.* \([0-9]\{1,\}\) .*$/\1/' \
+		>>/var/local/snmp/vmstat.tmp$$
 	mv /var/local/snmp/vmstat.tmp$$ /var/local/snmp/vmstat
 }
 
 # background the vmstat run
 vmstat &
-
